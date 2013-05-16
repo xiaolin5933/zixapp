a8d185ec26c0be69eaf4116d1e1510d23739286d
cain <ning.cai@yeepay.com>
2013-05-16 14:23:49 +0800

    tbl_crt.sh

--- sbin/tbl_crt.sh
+++ sbin/tbl_crt.sh
@@ -3,7 +3,7 @@
 cd $ZIXAPP_HOME/sql/table;
 
 db2 connect to $DB_NAME user $DB_USER using $DB_PASS;
-set current schema $DB_SCHEMA;
+db2 set current schema $DB_SCHEMA;
 for file in `find -type f`; do
     db2 -tvf $file;
 done
