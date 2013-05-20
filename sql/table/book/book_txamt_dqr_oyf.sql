670c14ba514a4cb11ca5c04c34c6c4a74f2d6300
cain <ning.cai@yeepay.com>
2013-05-20 17:04:53 +0800

    ../book/book_txamt_dqr.sql -> ../book/book_txamt_dqr_oyf.sql

--- sql/table/book/book_txamt_dqr_oyf.sql
+++ sql/table/book/book_txamt_dqr_oyf.sql
@@ -1,8 +1,8 @@
 -- 
 -- 2241.01 : 其他应付款-待确认交易款
 --
-drop table book_txamt_dqr;
-create table book_txamt_dqr (
+drop table book_txamt_dqr_oyf;
+create table book_txamt_dqr_oyf (
     -- 主键
     id                    bigint primary key not null,
 
@@ -24,31 +24,31 @@ create table book_txamt_dqr (
     ts_c	          timestamp default current timestamp
 );
 
-comment on table book_txamt_dqr            is '其他应付款-待确认交易款';
+comment on table book_txamt_dqr_oyf            is '其他应付款-待确认交易款';
 -- 核算项字典comment见 dict_book.sql
-comment on column book_txamt_dqr.ys_type   is '原始凭证类型';
-comment on column book_txamt_dqr.ys_id     is '原始凭证ID';
-comment on column book_txamt_dqr.jzpz_id   is '原始凭证类型';
-comment on column book_txamt_dqr.ts_c      is '创建时间';
+comment on column book_txamt_dqr_oyf.ys_type   is '原始凭证类型';
+comment on column book_txamt_dqr_oyf.ys_id     is '原始凭证ID';
+comment on column book_txamt_dqr_oyf.jzpz_id   is '原始凭证类型';
+comment on column book_txamt_dqr_oyf.ts_c      is '创建时间';
 
 -- id序列
-drop sequence seq_txamt_dqr;
-create sequence seq_txamt_dqr as bigint start with 1 increment by 1 minvalue 1 no maxvalue no cycle cache 200 order;
+drop sequence seq_txamt_dqr_oyf;
+create sequence seq_txamt_dqr_oyf as bigint start with 1 increment by 1 minvalue 1 no maxvalue no cycle cache 200 order;
 
 -- MQT表
-create table sum_txamt_dqr as (
+create table sum_txamt_dqr_oyf as (
     select bi as bi,
         period as period,
         tx_date as tx_date,
         sum(j)     as j,
         sum(d)     as d,
         count(*)   as cnt
-    from book_txamt_dqr
+    from book_txamt_dqr_oyf
     group by bi, period, tx_date
 )
 data initially deferred refresh immediate
 in tbs_dat;
 
 -- integrity unchecked
-set integrity for sum_txamt_dqr materialized query immediate unchecked;
+set integrity for sum_txamt_dqr_oyf materialized query immediate unchecked;
 
